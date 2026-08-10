a1b1e73d53b56dfa682a6559e7b3a6ca09404619
Gaetan <gaetan@walloftraders.com>
2026-08-10 17:10:59 +0200

    auto: models.py"

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ from enum import Enum
 from typing import Literal
 from uuid import uuid4
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, EmailStr, Field, model_validator
 
 
 # ============================================================
